8626373553b5d1da569023b0d9085661634d9c49
Jonathan Zhang <jonathanz28@nycstudents.net>
2022-10-05 23:59:04 -0400

    Unit1TipCalc

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,13 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         DecimalFormat df = new DecimalFormat("#.00");
 
+        //Here I create the variables needed
         double totalTip;
         double finalBill;
         double tipPerPerson;
         double totalPerPerson;
 
+        //Here I prompt the user for information about the bill, tip, and # of people
         System.out.println("Enter the total bill: ");
         double bill = scan.nextDouble();
 
@@ -20,15 +22,19 @@ public class Main {
         System.out.println("Enter the number of people: ");
         int people = scan.nextInt();
 
+        //Here I calculate the total tip
         totalTip = (tipPercent / 100.0) * bill;
         System.out.println("Total Tip Amount: $" + df.format(totalTip));
 
+        //Here I calculate the final bill including the tip
         finalBill = totalTip + bill;
         System.out.println("Total Bill Including Tip: $" + df.format(finalBill));
 
+        //Here I calculate the tip per person
         tipPerPerson = totalTip / people;
         System.out.println("Tip Per Person: $" + df.format(tipPerPerson));
 
+        //Here I calculate the total per person
         totalPerPerson = finalBill / people;
         System.out.println("Total Per Person: $" + df.format(totalPerPerson));
     }
